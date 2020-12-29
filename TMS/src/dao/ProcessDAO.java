2f7c1a79387d9ba2bca3d520532f85fbde33a05d
Xu-JiaJun <45767939+Xu-JiaJun@users.noreply.github.com>
2020-12-29 23:18:52 +0800

    test ignore

--- TMS/src/dao/ProcessDAO.java
+++ TMS/src/dao/ProcessDAO.java
@@ -5,13 +5,14 @@ import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import po.ProcessRecord;
 import po.PurchaseRecord;
 
 public class ProcessDAO {
 
     private Log log = LogFactory.getLog(ProcessDAO.class);
 
-    public void insertProcess(Process process){
+    public void insertProcess(ProcessRecord process){
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         Session session = sf.openSession();
         session.beginTransaction();
