64446ceba9baa34d7da30f037f8fbc5d8f1ebde4
Xu-JiaJun <45767939+Xu-JiaJun@users.noreply.github.com>
2021-01-07 12:45:25 +0800

    增加系统管理员

---
+++ TMS/web/pages/admin/admin.js
@@ -0,0 +1,18 @@
+//窗体加载完成后立即执行
+window.onload = function () {
+    changeCheckbox();
+}
+
+//需要对checkbox进行赋值，用于批量操作
+//需要验证分页后是否可用
+function changeCheckbox() {
+    let boxes = document.getElementsByName("checkList.checkList");
+    for (let i = 0; i < boxes.length; i++) {
+        let td1 = boxes[i].parentElement.parentElement;
+        //target即为seqID所在位置
+        let target = td1.nextElementSibling;
+        //console.table(target.innerHTML);
+        //将target内的内容赋给checkbox
+        boxes[i].value=target.innerHTML;
+    }
+}
