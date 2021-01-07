0c9e3247091c03f309291f80d34257bc706b3ac4
zy43 <76775641+zy43@users.noreply.github.com>
2021-01-07 17:54:16 +0800

    报废请求拦截器

---
+++ TMS/src/interceptor/fixApplyInterceptor.java
@@ -0,0 +1,37 @@
+package interceptor;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+import java.util.Map;
+
+    public class fixApplyInterceptor extends AbstractInterceptor {
+        public String intercept(ActionInvocation invocation) throws Exception {
+            System.out.println("fixApplyInterceptor executed!");
+            ActionContext ctx = invocation.getInvocationContext();
+            Map session = ctx.getSession();
+
+            //获取用户请求页面的参数
+            Map formMap = invocation.getInvocationContext().getParameters();
+//        Set set = formMap.keySet();
+//        for (Object key:set) {
+//            System.out.print("key:"+key.toString()+"   ");
+//            System.out.println(formMap.get(key));
+//        }
+            //获得输入来判空
+            String code = formMap.get("fixRecord.applyUID").toString();
+            String seqid = formMap.get("fixRecord.seqID").toString();
+            String des = formMap.get("fixRecord.description").toString();
+            String dealUID = formMap.get("fixRecord.dealUID").toString();
+            String Img = formMap.get("fixRecord.Img").toString();
+
+
+            //如果不为空则返回fixRecordNotnull
+            if (code!=null&&seqid!=null&&des!=null&&dealUID!=null&&Img!=null) {
+                return "fixRecordNotnull";
+            }
+            return invocation.invoke();
+        }
+    }
+
