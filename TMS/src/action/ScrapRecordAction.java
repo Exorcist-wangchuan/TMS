f8ad82e88e590fa39e04ee6ac553a707c55e5fa2
zy43 <76775641+zy43@users.noreply.github.com>
2021-01-10 20:33:12 +0800

    Update ScrapRecordAction.java

--- TMS/src/action/ScrapRecordAction.java
+++ TMS/src/action/ScrapRecordAction.java
@@ -52,9 +52,7 @@ public class ScrapRecordAction extends ActionSupport {
         this.scrapService = scrapService;
     }
 
-    public IProcessService getProcessService() {
-        return processService;
-    }
+
 
     public void setProcessService(IProcessService processService) {
         this.processService = processService;
